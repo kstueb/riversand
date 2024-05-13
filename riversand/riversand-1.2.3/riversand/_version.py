d3e21bde7341841febf0bb394fec7077c7c733b9
Konstanze Stübner <kstueb@gmail.com>
2024-05-13 17:11:55 +0200

    Add files via upload

---
+++ riversand/riversand-1.2.3/riversand/_version.py
@@ -0,0 +1,11 @@
+__version__ = "1.2.3"
+
+# print_defaults(), print_standardizations()
+# restandardize()
+
+# Point-based erosion rates and exposure ages
+# import_data(), get_textblock()
+# get_ages_from_server(), get_erates_from_server()
+
+
+
